80b68d8f12ef71037df7197748fe3f6b0e6267e3
suzukihaya <hayahayawork@gmail.com>
2023-07-11 16:21:03 +0900

    エブリデイシャッフリン

--- battle/src/App.tsx
+++ battle/src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import     { useState } from "react";
+import { styled } from "@mui/system";
+import slime from "./assets/slime.png";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface AnimatedImageProps {
+  animated: boolean;
 }
 
-export default App
+const Container = styled("div")({
+  background: "black",
+  height: "100vh",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+});
+
+const AnimatedImage = styled("img")<AnimatedImageProps>(({ animated }) => ({
+  transform: `scaleX(${animated ? -1 : 1})`,
+  transition: "transform 1s",
+}));
+
+function App() {
+  const [animated, setAnimated] = useState(false);
+
+  const handleButtonClick = () => {
+    setAnimated(!animated);
+  };
+
+  return (
+    <Container>
+      <AnimatedImage src={slime} width={"200"} height={"200"} animated={animated} />
+      <AnimatedImage src={slime} width={"200"} height={"200"} animated={!animated} />
+      <button onClick={handleButtonClick}>ボタン</button>
+    </Container>
+  );
+}
+
+export default App;
